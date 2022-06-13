cf42717de066903de791347663a6d530e2007f7d
aljsjunca <aljs.junca@gmail.com>
2022-06-13 15:11:02 -0300

    projeto em Spring Boot iniciado

---
+++ src/main/java/br/com/bytecompany/erp/controller/HomeController.java
@@ -0,0 +1,14 @@
+package br.com.bytecompany.erp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String homeController() {
+		return "index";
+	}
+	
+}
---
+++ src/main/java/br/com/bytecompany/erp/orm/Cliente.java
@@ -0,0 +1,42 @@
+package br.com.bytecompany.erp.orm;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	
+	private String nome;
+	private String cpf;
+	private String endereco;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		endereco = endereco;
+	}
+
+}
