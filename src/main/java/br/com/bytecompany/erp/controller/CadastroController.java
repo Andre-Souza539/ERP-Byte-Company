b4680fcd78a5fe17c29321b4c5dadd703dd80454
aljsjunca <aljs.junca@gmail.com>
2022-06-16 12:30:52 -0300

    primeira revisão do dia

--- src/main/java/br/com/bytecompany/erp/controller/CadastroController.java
+++ src/main/java/br/com/bytecompany/erp/controller/CadastroController.java
@@ -15,43 +15,40 @@ import br.com.bytecompany.erp.repository.ClienteRepository;
 
 @Controller
 public class CadastroController {
-	
+
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
+
 	@GetMapping("/cadastro")
 	public String cadastroForm(Model model) {
 		model.addAttribute("cliente", new Cliente());
 		return "cadastroForm";
 	}
 
-
 	@PostMapping("/salvar")
 	public String processForm(@Valid Cliente cliente, BindingResult result) {
-		if(result.hasErrors()) {
+		if (result.hasErrors()) {
 			return "cadastroForm";
 		}
 		clienteRepository.save(cliente);
 		return "redirect:/clientes";
 	}
-	
+
 	@GetMapping("/clientes/delete/{id}")
 	public String deleteCliente(@PathVariable Integer id) {
 		clienteRepository.deleteById(id);
 		return "redirect:/clientes";
 	}
-	
+
 	@GetMapping("/clientes/edit/{id}")
 	public String editCliente(@PathVariable Integer id, Model model) {
-		model.addAttribute("cliente", clienteRepository.findById(id));		
+		model.addAttribute("cliente", clienteRepository.findById(id));
 		return "updateForm";
 	}
-	
-	
+
 	@GetMapping("/save")
-	public String updateForm(Cliente cliente) {		
+	public String updateForm(Cliente cliente) {
 		clienteRepository.save(cliente);
 		return "redirect:/clientes";
 	}
 }
-	
